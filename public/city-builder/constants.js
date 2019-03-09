aeaf0898b86bfca8a173f4248818f144b6193a7d
Andrea Frittella <a.frittella@sintab.it>
2019-03-09 17:25:47 +0100

    Gabriel is a designer

--- public/city-builder/constants.js
+++ public/city-builder/constants.js
@@ -1,8 +1,8 @@
 canvasHeight = 400;
 
 const roomWidth = 16;
-const roomHeight = 12;
-const entranceHeight = 20;
-const roofHeight = 10;
+const roomHeight = 16;
+const entranceHeight = 30;
+const roofHeight = 20;
 const padding = 3;
 const buildingsPadding = 5;
